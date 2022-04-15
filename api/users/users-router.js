8f9e921e3af25098a40f0cab95016b5af4bca646
davedwight <dpdwight@gmail.com>
2022-04-14 21:30:25 -0600

    adds token and user verification

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
+const restricted = require("../middleware/restricted");
 const { confirmLoginFields, verifyLogin } = require("./users-middleware");
-
 const tokenBuilder = require("../middleware/token-builder");
 const Users = require("./users-model");
 
@@ -15,6 +15,25 @@ router.post("/", confirmLoginFields, verifyLogin, (req, res, next) => {
                 user_id,
                 token,
             });
+        })
+        .catch(next);
+});
+
+router.get("/:user_id", restricted, (req, res, next) => {
+    const { user_id } = req.params;
+    Users.getUserById(user_id)
+        .then((user) => {
+            const user_id = user.user_id;
+            if (user_id) {
+                res.status(200).json({
+                    message: `Token verified and user found`,
+                    user_id,
+                });
+            } else {
+                res.status(404).json({
+                    message: `User not found, please login`,
+                });
+            }
         })
         .catch(next);
 });
